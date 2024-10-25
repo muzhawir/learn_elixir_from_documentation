6a09b23fcc1d8362519db5d0979609ec0c197ac7
muzhawir <muzhawir.amri@gmail.com>
2024-10-25 17:15:05 +0800

    Initial Commit

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule LearnElixir.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :learn_elixir,
+      version: "0.1.0",
+      elixir: "~> 1.17",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:styler, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+    ]
+  end
+end

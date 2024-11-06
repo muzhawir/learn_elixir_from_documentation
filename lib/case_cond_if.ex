2a4edff71bd2c189eb4a0c6e281030d9f4eae4e9
muzhawir <muzhawir.amri@gmail.com>
2024-11-06 22:52:07 +0800

    05-cond

--- lib/case_cond_if.ex
+++ lib/case_cond_if.ex
@@ -32,4 +32,35 @@ defmodule CaseCondIf do
       _ -> "Invalid response"
     end
   end
+
+  @doc """
+  Uses `case` to match a value against multiple patterns until a matching one is found with a guard.
+
+  This function receives a tuple with exactly 2 elements: an atom status code, and a http status
+  code. Each pattern has a guard that checks if the status code ranges.
+
+  - `{:info, 100}` is for informational responses ranged from 100 to 199
+  - `{:ok, 200}` is for successful responses ranged from 200 to 299
+  - `{:redirect, 300}` is for redirection responses ranged from 300 to 399
+  - `{:client_error, 400}` is for client error responses ranged from 400 to 499
+  - `{:server_error, 500}` is for server error responses from 500 to 599
+
+  If none of these patterns match, the function will return `"Invalid response"`.
+
+  ## Examples
+
+    iex> CaseCondIf.case_match_with_clause({:ok, 200})
+    "Successful Response: 200"
+  """
+  @spec case_match_with_clause(tuple()) :: String.t()
+  def case_match_with_clause(tuple) when is_tuple(tuple) do
+    case tuple do
+      {:info, code} when code >= 100 and code <= 199 -> "Informational Response: #{code}"
+      {:ok, code} when code >= 200 and code <= 299 -> "Successful Response: #{code}"
+      {:redirect, code} when code >= 300 and code <= 399 -> "Redirection Response: #{code}"
+      {:client_error, code} when code >= 400 and code <= 499 -> "Client Error Response: #{code}"
+      {:server_error, code} when code >= 500 and code <= 599 -> "Server Error Response: #{code}"
+      _ -> "Invalid response"
+    end
+  end
 end
--- test/case_cond_if_test.exs
+++ test/case_cond_if_test.exs
@@ -2,4 +2,11 @@ defmodule CaseCondIfTest do
   use ExUnit.Case, async: true
 
   doctest CaseCondIf
+
+  describe "CaseCondIf.case_match_with_clause/1" do
+    test "should return correct output that matches with clause" do
+      assert CaseCondIf.case_match_with_clause({:ok, 200}) ===
+               "Successful Response: 200"
+    end
+  end
 end

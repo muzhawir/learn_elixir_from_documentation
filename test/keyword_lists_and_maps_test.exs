acd2c5cf8150c3d68f70e26f531c683858bf6031
Muzhawir Amri <muzhawir.amri@gmail.com>
2024-11-22 11:32:02 +0800

    Add Chapter 07: Keyword Lists And Maps

--- test/keyword_lists_and_maps_test.exs
+++ test/keyword_lists_and_maps_test.exs
@@ -2,4 +2,24 @@ defmodule KeywordListsAndMapsTest do
   use ExUnit.Case, async: true
 
   doctest KeywordListsAndMaps
+
+  describe "KeywordListsAndMaps.print_text/2" do
+    test "should print text" do
+      assert KeywordListsAndMaps.print_text("hello") === "hello"
+    end
+
+    test "should print text with upcase result" do
+      assert KeywordListsAndMaps.print_text("hello", upcase: true) === "HELLO"
+    end
+
+    test "should print text with capitalize result" do
+      assert KeywordListsAndMaps.print_text("hello", capitalize: true) === "Hello"
+    end
+
+    test "should raise an error if option :upcase and :capitalize are used at the same time" do
+      assert_raise KeyError, fn ->
+        KeywordListsAndMaps.print_text("hello", upcase: true, capitalize: true)
+      end
+    end
+  end
 end

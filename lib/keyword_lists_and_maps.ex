341cdbea251d948f80cd7ca5a46a464e855b644c
muzhawir <muzhawir.amri@gmail.com>
2024-11-22 11:29:58 +0800

    Add function print_username/1

--- lib/keyword_lists_and_maps.ex
+++ lib/keyword_lists_and_maps.ex
@@ -31,4 +31,19 @@ defmodule KeywordListsAndMaps do
       {true, true} -> raise ArgumentError.message(":upcase and :capitalize cannot be true at the same time")
     end
   end
+
+  @doc """
+  Print username from user map.
+
+  This function takes a user map as an argument and prints the username in uppercase.
+
+  ## Examples
+
+    iex> KeywordListsAndMaps.print_username(%{username: "joe", password: "secret"})
+    "JOE"
+  """
+  @spec print_username(map()) :: String.t()
+  def print_username(user) when is_map(user) do
+    print_text(user.username, upcase: true)
+  end
 end

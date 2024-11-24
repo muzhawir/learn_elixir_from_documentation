37c66c77375ccdc1b40153ff5d131a5d63a3e635
muzhawir <muzhawir.amri@gmail.com>
2024-11-24 15:46:26 +0800

    Refactor Chapter 07 - Binaries, Strings, and Charlists

--- lib/binaries_strings_and_charlists.ex
+++ lib/binaries_strings_and_charlists.ex
@@ -1,17 +1,19 @@
 defmodule BinariesStringsAndCharlists do
   @moduledoc """
-  Section Elixir Documentation #06 - Binaries, Strings, and Charlists
+  Section Elixir Documentation #07 - Binaries, Strings, and Charlists
   """
 
   @doc """
-  Checks if the given type is binary.
+  Checks if the given type is a binary.
 
-  This is one of many type-checking functions in Elixir that checks the type of a value. In this
-  case, `is_binary/1` checks if the value is a binary and returns a boolean.
+  This is a type-checking function in Elixir that checks if the given value is a binary,
+  returning a boolean value.
 
   ## Examples
-    iex> BinariesStringsAndCharlists.binary_type?("elixir")
-    true
+
+      iex> BinariesStringsAndCharlists.binary_type?("elixir")
+      true
+
   """
   @spec binary_type?(any()) :: boolean()
   def binary_type?(type) do
@@ -19,22 +21,16 @@ defmodule BinariesStringsAndCharlists do
   end
 
   @doc """
-  Returns the length and byte size of the given binary.
+  Returns the length and byte size of a binary.
 
-  This function demonstrates the use of the `String.length/1` and `byte_size/1` functions to
-  retrieve the length and byte size of a binary.
-
-  The length of a binary is the number of graphemes it contains, while the byte size is the number of bytes necessary to represent it.
-  In the example, the function takes a string "héllo" as input, which makes the length and byte
-  size different.
-
-  The length is 5 (the number of graphemes), and the byte size is 6 (the number
-  of bytes necessary to represent it, since the character "é" uses 2 bytes to represent).
-  This function returns a string that interpolates the result of length and byte size.
+  Demonstrates `String.length/1` for graphemes and `byte_size/1` for bytes.
+  For example, "héllo" has length 5 and byte size 6 due to "é" being 2 bytes.
 
   ## Examples
-    iex> BinariesStringsAndCharlists.binary_length_and_byte_size("héllo")
-    "The length of the binary is 5 and the byte size is 6"
+
+      iex> BinariesStringsAndCharlists.binary_length_and_byte_size("héllo")
+      "The length of the binary is 5 and the byte size is 6"
+
   """
   @spec binary_length_and_byte_size(binary()) :: String.t()
   def binary_length_and_byte_size(binary) when is_binary(binary) do
@@ -48,15 +44,16 @@ defmodule BinariesStringsAndCharlists do
   @doc """
   Returns the code points of the given emoji.
 
-  This function takes a binary as input and returns a list of code points if the input is a single
-  emoji. If the input is not a single emoji, the function raises an error.
+  This function takes a string with 1 length as input and returns a list of code points.
 
-  This function demonstrates the UTF-8 that proves a notion of grapheme. Graphemes may consist of
-  multiple Unicode code points that are represented as one.
+  This function illustrates the concept of graphemes in UTF-8 encoding. A grapheme may consist of
+  multiple Unicode code points that are combined to represent a single visible character. For
+  example, the emoji "👩‍🚒" has 3 code points: "👩", "‍", and "🚒".
 
   ## Examples
-    iex> BinariesStringsAndCharlists.emoji_code_point("👩‍🚒")
-    ["👩", "‍", "🚒"]
+      iex> BinariesStringsAndCharlists.emoji_code_point("👩‍🚒")
+      ["👩", "‍", "🚒"]
+
   """
   @spec emoji_code_point(binary()) :: [binary()] | String.t()
   def emoji_code_point(emoji) when is_binary(emoji) do
@@ -68,14 +65,16 @@ defmodule BinariesStringsAndCharlists do
   end
 
   @doc """
-  Returns the binary representation of the given binary concatenated with a null byte.
+  Appends a null byte to the given string.
 
-  This function takes a binary (like string or emoji) as input and returns a binary that is the same as the input
-  concatenated with a null byte.
+  This function takes a string as input and returns a raw form of bitstring with a null
+  byte appended to it.
 
   ## Examples
-    iex> BinariesStringsAndCharlists.binary_representation("hełło")
-    <<104, 101, 197, 130, 197, 130, 111, 0>>
+
+      iex> BinariesStringsAndCharlists.binary_representation("hełło")
+      <<104, 101, 197, 130, 197, 130, 111, 0>>
+
   """
   @spec binary_representation(binary()) :: binary()
   def binary_representation(binary) when is_binary(binary) do
